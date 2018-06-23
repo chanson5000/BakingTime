0359a48c8da61aecfe569c0ae621d51c7bc61c40
Cory <chanson5000@gmail.com>
2018-06-22 22:21:41 -0500

    Fixed some mistakes.

--- app/src/main/java/com/nverno/bakingtime/repository/RecipeDao.java
+++ app/src/main/java/com/nverno/bakingtime/repository/RecipeDao.java
@@ -1,16 +1,17 @@
 package com.nverno.bakingtime.repository;
 
 import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
-import android.graphics.Movie;
 
 import com.nverno.bakingtime.model.Recipe;
 
 import java.util.List;
 
+@Dao
 public interface RecipeDao {
 
     @Query("SELECT * FROM recipe")
@@ -20,6 +21,6 @@ public interface RecipeDao {
     void insertMany(List<Recipe> recipes);
 
     @Delete
-    void delete(Movie movie);
+    void delete(Recipe recipe);
 
 }
