dc03ccb971a5c21e8ce5c3fa357a8d837e141d93
Cory <chanson5000@gmail.com>
2018-06-22 21:33:34 -0500

    Added initial data access object.

--- app/src/main/java/com/nverno/bakingtime/repository/RecipeDao.java
+++ app/src/main/java/com/nverno/bakingtime/repository/RecipeDao.java
@@ -1,7 +1,25 @@
 package com.nverno.bakingtime.repository;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.graphics.Movie;
+
+import com.nverno.bakingtime.model.Recipe;
+
+import java.util.List;
+
 public interface RecipeDao {
 
+    @Query("SELECT * FROM recipe")
+    LiveData<List<Recipe>> getAll();
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertMany(List<Recipe> recipes);
+
+    @Delete
+    void delete(Movie movie);
 
 }
