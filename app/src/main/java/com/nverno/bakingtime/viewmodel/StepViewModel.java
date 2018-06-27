3f85134f0e7f69be8c6a978828081fc880cf3289
Cory <chanson5000@gmail.com>
2018-06-27 13:24:58 -0500

    Added steps view model.

---
+++ app/src/main/java/com/nverno/bakingtime/viewmodel/StepViewModel.java
@@ -0,0 +1,35 @@
+package com.nverno.bakingtime.viewmodel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.support.annotation.NonNull;
+
+import com.nverno.bakingtime.model.Step;
+import com.nverno.bakingtime.repository.StepRepository;
+
+import java.util.List;
+
+public class StepViewModel extends AndroidViewModel {
+
+    private LiveData<List<Step>> steps;
+
+    StepRepository stepRepository;
+
+    public StepViewModel(@NonNull Application app) {
+        super(app);
+
+        stepRepository = new StepRepository(this.getApplication());
+
+        steps = new MutableLiveData<>();
+    }
+
+    public LiveData<List<Step>> stepsForRecipe() {
+        return steps;
+    }
+
+    public void setSelectedRecipe(int recipeId) {
+        steps = stepRepository.getStepsForRecipe(recipeId);
+    }
+}
