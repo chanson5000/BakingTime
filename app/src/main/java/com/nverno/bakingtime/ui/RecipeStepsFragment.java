a3058ca643a43b40328173b779af6e7d18ceb3ab
Cory <chanson5000@gmail.com>
2018-06-27 19:03:16 -0500

    Almost working fragment with view model.

--- app/src/main/java/com/nverno/bakingtime/ui/RecipeStepsFragment.java
+++ app/src/main/java/com/nverno/bakingtime/ui/RecipeStepsFragment.java
@@ -1,7 +1,113 @@
 package com.nverno.bakingtime.ui;
 
-import android.app.Fragment;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class RecipeStepsFragment extends Fragment {
+import com.nverno.bakingtime.R;
+import com.nverno.bakingtime.adapter.RecipeStepOnClickHandler;
+import com.nverno.bakingtime.adapter.RecipeStepsAdapter;
+import com.nverno.bakingtime.model.Ingredient;
+import com.nverno.bakingtime.model.Step;
+import com.nverno.bakingtime.viewmodel.RecipeViewModel;
+
+import java.util.List;
+
+public class RecipeStepsFragment extends Fragment
+        implements RecipeStepOnClickHandler {
+
+    private RecipeStepOnClickHandler mClickHandler;
+    private Context mContext;
+    RecipeViewModel recipeViewModel;
+
+    private RecyclerView mRecyclerView;
+    private RecipeStepsAdapter mStepsAdapter;
+
+    private static final String RECIPE_ID = "RECIPE_ID";
+
+    private TextView mTextIngredients;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        mContext = context;
+
+
+        try {
+            mClickHandler = (RecipeStepOnClickHandler) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+                    + " must implement RecipeStepOnClickHandler");
+        }
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    public RecipeStepsFragment() {
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        recipeViewModel = ViewModelProviders.of(getActivity()).get(RecipeViewModel.class);
+
+        final View rootView = inflater.inflate(R.layout.recipe_steps_fragment, container, false);
+
+        mTextIngredients = rootView.findViewById(R.id.recipe_steps_ingredients);
+
+        recipeViewModel.getSelectedRecipeIngredients().observe(this, new Observer<List<Ingredient>>() {
+            @Override
+            public void onChanged(@Nullable List<Ingredient> ingredients) {
+                if (ingredients != null && !ingredients.isEmpty()) {
+                    StringBuilder ingredientString = new StringBuilder();
+
+                    for (int i = 0; i < ingredients.size(); i++) {
+                        if (i == ingredients.size() - 1) {
+                            ingredientString.append(ingredients.get(i).getName()).append(".");
+                        }
+                        ingredientString.append(ingredients.get(i).getName()).append(", ");
+                    }
+
+                    String textToSet = ingredientString.toString();
+
+                    mTextIngredients.setText(textToSet);
+                }
+            }
+        });
+
+        mRecyclerView = rootView.findViewById(R.id.recipe_steps_recycler_view);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mStepsAdapter = new RecipeStepsAdapter(mClickHandler);
+
+        mRecyclerView.setAdapter(mStepsAdapter);
+
+        return rootView;
+    }
+
+    public void onStepClick(Step step) {
+
+    }
 
 }
