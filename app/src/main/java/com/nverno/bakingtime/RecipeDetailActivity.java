f0fc40de81b2799d06071247beff6125adb86cf2
Cory <chanson5000@gmail.com>
2018-06-27 19:02:47 -0500

    Initial working recipe detail activity.

--- app/src/main/java/com/nverno/bakingtime/RecipeDetailActivity.java
+++ app/src/main/java/com/nverno/bakingtime/RecipeDetailActivity.java
@@ -1,11 +1,56 @@
 package com.nverno.bakingtime;
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.nverno.bakingtime.adapter.OnClickHandler;
+import com.nverno.bakingtime.adapter.RecipeStepOnClickHandler;
+import com.nverno.bakingtime.model.Recipe;
+import com.nverno.bakingtime.model.Step;
+import com.nverno.bakingtime.ui.RecipeStepsFragment;
+import com.nverno.bakingtime.viewmodel.RecipeViewModel;
 
-public class RecipeDetailActivity extends AppCompatActivity implements OnClickHandler {
 
-    private
+public class RecipeDetailActivity extends AppCompatActivity
+        implements RecipeStepOnClickHandler {
 
+    private static Recipe mRecipe;
+    private static int mRecipeId;
+
+    private static final String RECIPE_ID = "RECIPE_ID";
+
+    RecipeStepsFragment recipeStepsFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_detail);
+
+        Intent parentIntent = getIntent();
+
+        recipeStepsFragment = new RecipeStepsFragment();
+
+        if (parentIntent != null
+                && parentIntent.hasExtra(RECIPE_ID)) {
+
+            Bundle bundle = parentIntent.getExtras();
+
+            if (bundle != null) {
+                initViewModel(bundle.getInt(RECIPE_ID));
+            }
+        }
+    }
+
+    private void initViewModel(int recipeId) {
+
+        RecipeViewModel recipeViewModel = ViewModelProviders.of(this)
+                .get(RecipeViewModel.class);
+
+        recipeViewModel.setSelectedRecipe(recipeId);
+    }
+
+    public void onStepClick(Step step) {
+
+    }
 }
