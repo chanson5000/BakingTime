d9056e04dae1a4e2b258f2df121b13ccfe060295
Cory <chanson5000@gmail.com>
2018-06-28 13:12:40 -0500

    Added indexing for foreign keys.

--- app/src/main/java/com/nverno/bakingtime/model/Ingredient.java
+++ app/src/main/java/com/nverno/bakingtime/model/Ingredient.java
@@ -2,6 +2,7 @@ package com.nverno.bakingtime.model;
 
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.ForeignKey;
+import android.arch.persistence.room.Index;
 import android.arch.persistence.room.PrimaryKey;
 
 import com.google.gson.annotations.Expose;
@@ -13,7 +14,8 @@ import static android.arch.persistence.room.ForeignKey.CASCADE;
         entity = Recipe.class,
         parentColumns = "id",
         childColumns = "recipeId",
-        onDelete = CASCADE))
+        onDelete = CASCADE),
+        indices = {@Index("recipeId")})
 public class Ingredient {
 
     @PrimaryKey(autoGenerate = true)
