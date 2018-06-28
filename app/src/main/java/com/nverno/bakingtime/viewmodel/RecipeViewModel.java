c1b3baf27bd63a48a8e29663a80865a78f2bdde1
Cory <chanson5000@gmail.com>
2018-06-28 13:13:46 -0500

    Showing alternative ways to fetch live data. Keeping comments for now.

--- app/src/main/java/com/nverno/bakingtime/viewmodel/RecipeViewModel.java
+++ app/src/main/java/com/nverno/bakingtime/viewmodel/RecipeViewModel.java
@@ -5,6 +5,7 @@ import android.arch.core.util.Function;
 import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MediatorLiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.Transformations;
 import android.support.annotation.NonNull;
 
@@ -19,13 +20,14 @@ import java.util.List;
 public class RecipeViewModel extends AndroidViewModel {
 
     private final LiveData<List<Recipe>> recipes;
-    private final LiveData<List<Ingredient>> ingredients;
+//    private final LiveData<List<Ingredient>> ingredients;
+//    private final LiveData<List<Step>> steps;
 
     private LiveData<Recipe> selectedRecipe;
     private LiveData<List<Step>> selectedRecipeSteps;
     private LiveData<List<Ingredient>> selectedRecipeIngredients;
 
-    RecipeRepository recipeRepository;
+    private RecipeRepository recipeRepository;
 
     public RecipeViewModel(@NonNull Application app) {
         super(app);
@@ -33,7 +35,8 @@ public class RecipeViewModel extends AndroidViewModel {
         recipeRepository = new RecipeRepository(this.getApplication());
 
         recipes = recipeRepository.getAll();
-        ingredients = recipeRepository.getAllIngredients();
+//        ingredients = recipeRepository.getAllIngredients();
+//        steps = new MutableLiveData<>();
 
         selectedRecipe = new MediatorLiveData<>();
         selectedRecipeSteps = new MediatorLiveData<>();
@@ -69,21 +72,27 @@ public class RecipeViewModel extends AndroidViewModel {
             }
         });
 
-        selectedRecipeIngredients = Transformations.map(ingredients, new Function<List<Ingredient>, List<Ingredient>>() {
-            @Override
-            public List<Ingredient> apply(List<Ingredient> ingredients) {
+        selectedRecipeIngredients = recipeRepository.getIngredientsForRecipe(recipeId);
 
-                List<Ingredient> selectedIngredients = new ArrayList<>();
+        // ** The alternative way...
 
-                for (Ingredient ingredient : ingredients) {
-                    if (ingredient.getRecipeId() == recipeId) {
-                        selectedIngredients.add(ingredient);
-                    }
-                }
+//        selectedRecipeIngredients = Transformations.map(ingredients, new Function<List<Ingredient>, List<Ingredient>>() {
+//            @Override
+//            public List<Ingredient> apply(List<Ingredient> ingredients) {
+//
+//                List<Ingredient> selectedIngredients = new ArrayList<>();
+//
+//                for (Ingredient ingredient : ingredients) {
+//                    if (ingredient.getRecipeId() == recipeId) {
+//                        selectedIngredients.add(ingredient);
+//                    }
+//                }
+//
+//                return selectedIngredients;
+//            }
+//        });
 
-                return selectedIngredients;
-            }
-        });
+        selectedRecipeSteps = recipeRepository.getStepsForRecipe(recipeId);
     }
 
 }
