02f188ca8165c4bc4babc659376c963944930866
Cory <chanson5000@gmail.com>
2018-06-30 18:23:58 -0500

    Removed un-needed fragment references.

--- app/src/main/java/com/nverno/bakingtime/RecipeDetailActivity.java
+++ app/src/main/java/com/nverno/bakingtime/RecipeDetailActivity.java
@@ -3,15 +3,10 @@ package com.nverno.bakingtime;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.nverno.bakingtime.adapter.RecipeStepOnClickHandler;
 import com.nverno.bakingtime.model.Step;
-import com.nverno.bakingtime.ui.RecipeMediaFragment;
-import com.nverno.bakingtime.ui.RecipeStepDetailFragment;
-import com.nverno.bakingtime.ui.RecipeStepsFragment;
 import com.nverno.bakingtime.viewmodel.RecipeViewModel;
 
 
@@ -24,12 +19,6 @@ public class RecipeDetailActivity extends AppCompatActivity
     private boolean mSmallLayout;
 
     private RecipeViewModel recipeViewModel;
-
-    FragmentManager fragmentManager;
-
-    Fragment mRecipeStepDetailFragment;
-    Fragment mRecipeStepsFragment;
-    Fragment mRecipeMediaFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,14 +35,9 @@ public class RecipeDetailActivity extends AppCompatActivity
             if (bundle != null) {
                 initViewModel(bundle.getInt(RECIPE_ID));
             }
+        } else {
+            initViewModel();
         }
-
-        fragmentManager = getSupportFragmentManager();
-
-        mRecipeStepDetailFragment = new RecipeStepDetailFragment();
-        mRecipeStepsFragment = new RecipeStepsFragment();
-        mRecipeMediaFragment = new RecipeMediaFragment();
-
 
         if (findViewById(R.id.divider_vertical_constraint) != null) {
             mSmallLayout = true;
@@ -61,19 +45,9 @@ public class RecipeDetailActivity extends AppCompatActivity
 
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        if (mSmallLayout) {
-            fragmentManager.beginTransaction()
-                    .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
-                    .show(mRecipeStepsFragment)
-                    .hide(mRecipeMediaFragment)
-                    .hide(mRecipeStepDetailFragment)
-                    .commit();
-        }
-
+    private void initViewModel() {
+        recipeViewModel = ViewModelProviders.of(this)
+                .get(RecipeViewModel.class);
     }
 
     private void initViewModel(int recipeId) {
@@ -88,18 +62,8 @@ public class RecipeDetailActivity extends AppCompatActivity
     }
 
     public void onStepClick(Step step) {
-
         if (mSmallLayout) {
-            fragmentManager.beginTransaction()
-                    .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
-                    .hide(mRecipeStepsFragment)
-                    .show(mRecipeMediaFragment)
-                    .show(mRecipeStepDetailFragment)
-                    .commit();
-
             Intent intent = new Intent(this, RecipeStepDetailActivity.class);
-
-//            Step selectedRecipeStep = recipeViewModel.getSelectedRecipeStep().getValue();
 
             if (step != null) {
                 intent.putExtra(RECIPE_ID, step.getRecipeId());
