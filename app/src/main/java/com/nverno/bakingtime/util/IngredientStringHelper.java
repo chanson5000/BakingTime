0b8a41c6c3a6fbb4a942dabb44b1cd6f2ae6f3b0
Cory <chanson5000@gmail.com>
2018-07-12 19:53:25 -0500

    Added helper class for strings and widget interaction.

--- app/src/main/java/com/nverno/bakingtime/util/IngredientStringHelper.java
+++ app/src/main/java/com/nverno/bakingtime/util/IngredientStringHelper.java
@@ -1,12 +1,51 @@
 package com.nverno.bakingtime.util;
 
+import android.content.Context;
+
+import com.nverno.bakingtime.RecipeIngredientsWidget;
+import com.nverno.bakingtime.WidgetUpdateService;
 import com.nverno.bakingtime.model.Ingredient;
 
 import java.util.List;
 
 public class IngredientStringHelper {
 
-    public static String ListToFormattedString(List<Ingredient> ingredients) {
+    private static IngredientStringHelper INSTANCE = null;
+    private String mCurrentIngredients;
+    private String mCurrentRecipeName;
+
+    private IngredientStringHelper() {}
+
+    public static IngredientStringHelper getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new IngredientStringHelper();
+        }
+        return INSTANCE;
+    }
+
+    public String getCurrentIngredientsString() {
+        if (mCurrentIngredients != null) {
+            return mCurrentIngredients;
+        } else {
+            return null;
+        }
+    }
+
+    public String getCurrentRecipeName() {
+        if (mCurrentRecipeName != null) {
+            return mCurrentRecipeName;
+        } else {
+            return null;
+        }
+    }
+
+    public void setCurrentIngredientsString(Context context, String currentIngredients) {
+        mCurrentIngredients = currentIngredients;
+
+        WidgetUpdateService.startActionUpdateRecipeWidgets(context);
+    }
+
+    public String ListToFormattedString(Context context, List<Ingredient> ingredients) {
         StringBuilder ingredientString = new StringBuilder();
 
         ingredientString.append("Ingredients: ");
@@ -19,8 +58,16 @@ public class IngredientStringHelper {
             }
         }
 
-        String textToSet = ingredientString.toString();
+        String currentIngredients = ingredientString.toString();
 
-        return textToSet;
+        getInstance().setCurrentIngredientsString(context, currentIngredients);
+
+        return currentIngredients;
+    }
+
+    public void setCurrentRecipename(Context context, String currentRecipeName) {
+        mCurrentRecipeName = currentRecipeName;
+
+        WidgetUpdateService.startActionUpdateRecipeWidgets(context);
     }
 }
