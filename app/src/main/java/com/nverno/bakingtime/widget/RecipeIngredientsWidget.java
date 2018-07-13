43ade6aa8e644dd848479fa023f66629b0a442e7
Cory <chanson5000@gmail.com>
2018-07-13 15:10:07 -0500

    Modified launching intent with a back stack now in case it is launched without an existing back stack.

--- app/src/main/java/com/nverno/bakingtime/widget/RecipeIngredientsWidget.java
+++ app/src/main/java/com/nverno/bakingtime/widget/RecipeIngredientsWidget.java
@@ -1,6 +1,7 @@
 package com.nverno.bakingtime.widget;
 
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.widget.RemoteViews;
 
 import com.nverno.bakingtime.R;
+import com.nverno.bakingtime.ui.MainActivity;
 import com.nverno.bakingtime.ui.RecipeDetailActivity;
 import com.nverno.bakingtime.model.Recipe;
 
@@ -29,6 +31,10 @@ public class RecipeIngredientsWidget extends AppWidgetProvider {
         RemoteViews views = new RemoteViews(context.getPackageName(),
                 R.layout.recipe_ingredients_widget);
 
+
+        // The pending intent
+        Intent intent;
+
         if (recipe != null && ingredients != null) {
             recipeNameText = recipe.getName();
             ingredientsText = ingredients;
@@ -42,18 +48,29 @@ public class RecipeIngredientsWidget extends AppWidgetProvider {
             views.setViewVisibility(R.id.widget_text_recipe_name, View.VISIBLE);
             views.setViewVisibility(R.id.widget_text_static_ingredients, View.VISIBLE);
             views.setViewVisibility(R.id.widget_text_ingredients_list, View.VISIBLE);
+
+            intent = new Intent(context, RecipeDetailActivity.class);
+            intent.putExtra(RECIPE_ID, recipe.getId());
         } else {
             views.setViewVisibility(R.id.widget_text_recipe_name, View.GONE);
             views.setViewVisibility(R.id.widget_text_static_ingredients, View.GONE);
             views.setViewVisibility(R.id.widget_text_ingredients_list, View.GONE);
             views.setViewVisibility(R.id.widget_text_static_no_recipe_selected, View.VISIBLE);
+
+            intent = new Intent(context, MainActivity.class);
         }
 
-        // The pending intent
-        Intent intent = new Intent(context, RecipeDetailActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
-                intent, 0);
+        // This is the vanilla way to create a pending intent.
+        // PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
+        // intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // This way adds a back stack in case the widget has launched the activity on its own
+        // without an already existing back stack.
+        PendingIntent pendingIntent =
+                TaskStackBuilder.create(context).addNextIntentWithParentStack(intent)
+                .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
 
         // Widgets allow click handlers to only launch pending intents
         views.setOnClickPendingIntent(R.id.widget_linear_layout, pendingIntent);
