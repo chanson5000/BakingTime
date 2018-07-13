2a74cab254d2e491a0c75f46592bebd401bd4019
Cory <chanson5000@gmail.com>
2018-07-12 19:55:22 -0500

    Added functionality for widget update service. Added appropriate code for recipe name text view.

--- app/src/main/java/com/nverno/bakingtime/RecipeIngredientsWidget.java
+++ app/src/main/java/com/nverno/bakingtime/RecipeIngredientsWidget.java
@@ -1,22 +1,52 @@
 package com.nverno.bakingtime;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
+
+import com.nverno.bakingtime.model.Ingredient;
+import com.nverno.bakingtime.util.IngredientStringHelper;
+
+import java.util.List;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class RecipeIngredientsWidget extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, String recipeName,
+                                String ingredients, int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        String recipeNameText;
+        String ingredientsText;
+
+        if (recipeName != null) {
+            recipeNameText = recipeName;
+        } else {
+            recipeNameText = context.getString(R.string.no_recipe_selected);
+        }
+
+        if (ingredients != null) {
+            ingredientsText = ingredients;
+        } else {
+            ingredientsText = "";
+        }
+
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_ingredients_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setTextViewText(R.id.ingredients_widget_text, ingredientsText);
+        views.setTextViewText(R.id.ingredients_widget_recipe_name, recipeNameText);
+
+        // The pending intent
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
+                intent, 0);
+
+        // Widgets allow click handlers to only launch pending intents
+        views.setOnClickPendingIntent(R.id.ingredients_widget_text, pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -27,9 +57,21 @@ public class RecipeIngredientsWidget extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         // Always expect multiple instances exist and update them
+
+        WidgetUpdateService.startActionUpdateRecipeWidgets(context);
+    }
+
+    public static void updateRecipeIngredientsWidgets(Context context, AppWidgetManager appWidgetManager,
+                                                      String recipeName, String ingredients,
+                                                      int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+            updateAppWidget(context, appWidgetManager, recipeName, ingredients, appWidgetId);
         }
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        // Perform action when one or more AppWidget instances have been deleted
     }
 
     @Override
---
+++ app/src/main/java/com/nverno/bakingtime/WidgetUpdateService.java
@@ -0,0 +1,50 @@
+package com.nverno.bakingtime;
+
+import android.app.IntentService;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+
+import com.nverno.bakingtime.util.IngredientStringHelper;
+
+public class WidgetUpdateService extends IntentService {
+
+    public static final String ACTION_UPDATE_RECIPE_WIDGETS = "com.nverno.bakingtime.action.update_recipe_widgets";
+
+    public WidgetUpdateService() {
+        super("WidgetUpdateService");
+    }
+
+    public static void startActionUpdateRecipeWidgets(Context context) {
+        Intent intent = new Intent(context, WidgetUpdateService.class);
+        intent.setAction(ACTION_UPDATE_RECIPE_WIDGETS);
+        context.startService(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_UPDATE_RECIPE_WIDGETS.equals(action)) {
+                handleActionUpdateRecipeIngredientsWidget();
+            }
+        }
+    }
+
+    private void handleActionUpdateRecipeIngredientsWidget() {
+
+        String recipeName = IngredientStringHelper.getInstance().getCurrentRecipeName();
+
+        String ingredients = IngredientStringHelper.getInstance().getCurrentIngredientsString();
+
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(
+                new ComponentName(this, RecipeIngredientsWidget.class));
+        // Update all widgets
+        RecipeIngredientsWidget.updateRecipeIngredientsWidgets(this, appWidgetManager, recipeName,
+                ingredients, appWidgetIds);
+
+    }
+
+}
