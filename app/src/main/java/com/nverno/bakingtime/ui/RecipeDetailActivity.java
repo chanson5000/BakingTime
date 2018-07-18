c5ad2852d274c710837b4203bdb90da7a559a632
Cory <chanson5000@gmail.com>
2018-07-18 18:51:21 -0500

    Fixed problem with resetting of the ViewModel.

--- app/src/main/java/com/nverno/bakingtime/ui/RecipeDetailActivity.java
+++ app/src/main/java/com/nverno/bakingtime/ui/RecipeDetailActivity.java
@@ -1,24 +1,18 @@
 package com.nverno.bakingtime.ui;
 
-import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.nverno.bakingtime.R;
-import com.nverno.bakingtime.model.Recipe;
 import com.nverno.bakingtime.viewmodel.RecipeViewModel;
 
 public class RecipeDetailActivity extends AppCompatActivity {
 
     private static final String RECIPE_ID = "RECIPE_ID";
-    private static final String STEP_ID = "STEP_ID";
 
-    private boolean mSmallLayout;
-
-    private RecipeViewModel recipeViewModel;
+    private RecipeViewModel mRecipeViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,41 +21,36 @@ public class RecipeDetailActivity extends AppCompatActivity {
 
         Intent parentIntent = getIntent();
 
-        if (parentIntent != null
-                && parentIntent.hasExtra(RECIPE_ID)) {
-
+        if (parentIntent != null && parentIntent.hasExtra(RECIPE_ID)) {
             Bundle bundle = parentIntent.getExtras();
-
             if (bundle != null) {
                 initViewModel(bundle.getInt(RECIPE_ID));
             }
+            // Remove extras to prevent resetting our ViewModel.
+            parentIntent.removeExtra(RECIPE_ID);
+
         } else {
             initViewModel();
-        }
-
-        if (findViewById(R.id.divider_vertical_constraint) != null) {
-            mSmallLayout = true;
         }
     }
 
     private void initViewModel() {
-        recipeViewModel = ViewModelProviders.of(this)
-                .get(RecipeViewModel.class);
+        mRecipeViewModel = ViewModelProviders.of(this).get(RecipeViewModel.class);
+
+        mRecipeViewModel.getSelectedRecipe().observe(this, recipe -> {
+            if (recipe != null) {
+                setTitle(recipe.getName() + " Recipe");
+            }
+        });
     }
 
-    private void initViewModel(final int recipeId) {
-        recipeViewModel = ViewModelProviders.of(this)
-                .get(RecipeViewModel.class);
+    private void initViewModel(int recipeId) {
+        mRecipeViewModel = ViewModelProviders.of(this).get(RecipeViewModel.class);
 
-        recipeViewModel.setSelectedRecipe(recipeId);
+        mRecipeViewModel.setSelectedRecipe(recipeId);
+        mRecipeViewModel.setSelectedRecipeStep(0);
 
-        // If we have no selected recipe yet, default to 0 (Introduction).
-        if (recipeViewModel.getSelectedRecipeStep().getValue() == null) {
-            recipeViewModel.setSelectedRecipeStep(0);
-        }
-
-        // Setting the title/action bar to the recipe name.
-        recipeViewModel.getSelectedRecipe().observe(this, recipe -> {
+        mRecipeViewModel.getSelectedRecipe().observe(this, recipe -> {
             if (recipe != null) {
                 setTitle(recipe.getName() + " Recipe");
             }
