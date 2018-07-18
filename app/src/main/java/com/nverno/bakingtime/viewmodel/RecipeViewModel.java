df43119b0c7661f1298e864d1fc78f0209ca69d7
Cory <chanson5000@gmail.com>
2018-07-18 17:41:32 -0500

    Got the recipeViewModel exactly how I want it at this point. Finally figured out how well the Transformations work.

--- app/src/main/java/com/nverno/bakingtime/viewmodel/RecipeViewModel.java
+++ app/src/main/java/com/nverno/bakingtime/viewmodel/RecipeViewModel.java
@@ -3,7 +3,6 @@ package com.nverno.bakingtime.viewmodel;
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.MediatorLiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.Transformations;
 import android.support.annotation.NonNull;
@@ -19,114 +18,72 @@ public class RecipeViewModel extends AndroidViewModel {
 
     private RecipeRepository recipeRepository;
 
-    private LiveData<List<Recipe>> recipes;
-    private LiveData<Recipe> selectedRecipe;
-    private LiveData<Step> selectedRecipeStep;
+    private MutableLiveData<Integer> selectedRecipeId = new MutableLiveData<>();
+    private MutableLiveData<Integer> selectedStepId = new MutableLiveData<>();
 
-    private LiveData<List<Step>> selectedRecipeSteps;
-    private LiveData<List<Ingredient>> selectedRecipeIngredients;
+    private LiveData<Recipe> selectedRecipe =
+            Transformations.switchMap(selectedRecipeId,
+                    recipeId -> recipeRepository.getRecipeById(recipeId));
+
+    private LiveData<List<Step>> selectedRecipeSteps
+            = Transformations.map(selectedRecipe, Recipe::getSteps);
+
+    private LiveData<List<Ingredient>> selectedRecipeIngredients =
+            Transformations.map(selectedRecipe, Recipe::getIngredients);
+
+    private LiveData<Step> selectedRecipeStep
+            = Transformations.switchMap(selectedStepId,
+            stepId -> Transformations.map(selectedRecipeSteps, steps -> {
+        for (Step step : steps) {
+            if (step.getStepNumber() == stepId) {
+                return step;
+            }
+        }
+        return null;
+    }));
 
     public RecipeViewModel(@NonNull Application app) {
         super(app);
-
         recipeRepository = new RecipeRepository(this.getApplication());
-
-        recipes = recipeRepository.getAll();
     }
 
-    public void setSelectedRecipe(final int recipeId) {
-        selectedRecipe = recipeRepository.getRecipeById(recipeId);
-        setSelectedRecipeSteps();
-        setSelectedRecipeIngredients();
+    public void setSelectedRecipe(int recipeId) {
+        selectedRecipeId.setValue(recipeId);
     }
 
-    private void setSelectedRecipeSteps() {
-        selectedRecipeSteps = Transformations.map(selectedRecipe, Recipe::getSteps);
-    }
-
-    private void setSelectedRecipeIngredients() {
-        selectedRecipeIngredients = Transformations.map(selectedRecipe, Recipe::getIngredients);
-    }
-
-    public void setSelectedRecipeStep(final int recipeStepNumber) {
-        selectedRecipeStep = Transformations.map(selectedRecipeSteps, steps -> {
-            for (Step step : steps) {
-                if (step.getStepNumber() == recipeStepNumber) {
-                    return step;
-                }
-            }
-            return null;
-        });
+    public void setSelectedRecipeStep(int recipeStepNumber) {
+        selectedStepId.setValue(recipeStepNumber);
     }
 
     public LiveData<List<Recipe>> getRecipes() {
-        if (recipes == null) {
-            recipes = new MediatorLiveData<>();
-        }
-        return recipes;
+        return recipeRepository.getAll();
     }
 
     public LiveData<Recipe> getSelectedRecipe() {
-        if (selectedRecipe == null) {
-            selectedRecipe = new MediatorLiveData<>();
-        }
         return selectedRecipe;
     }
 
     public LiveData<List<Step>> getSelectedRecipeSteps() {
-        if (selectedRecipeSteps == null) {
-            selectedRecipe = new MediatorLiveData<>();
-        }
         return selectedRecipeSteps;
     }
 
     public LiveData<List<Ingredient>> getSelectedRecipeIngredients() {
-        if (selectedRecipeIngredients == null) {
-            selectedRecipeIngredients = new MediatorLiveData<>();
-        }
         return selectedRecipeIngredients;
     }
 
     public LiveData<Step> getSelectedRecipeStep() {
-        if (selectedRecipeStep == null) {
-            selectedRecipeStep = new MediatorLiveData<>();
-        }
         return selectedRecipeStep;
     }
 
-    private void incrementRecipeStep() {
-        if (selectedRecipeStep.getValue() == null) {
-            return;
-        }
-        selectedRecipeStep = Transformations.map(selectedRecipeSteps, steps -> {
-            for (Step step : steps) {
-                if (step.getStepNumber() == selectedRecipeStep.getValue().getStepNumber() - 1) {
-                    return step;
-                }
-            }
-            return null;
-        });
-    }
-
-    private void decrementRecipeStep() {
-        if (selectedRecipeStep.getValue() == null) {
-            return;
-        }
-        selectedRecipeStep = Transformations.map(selectedRecipeSteps, steps -> {
-            for (Step step : steps) {
-                if (step.getStepNumber() == selectedRecipeStep.getValue().getStepNumber() - 1 ) {
-                    return step;
-                }
-            }
-            return null;
-        });
-    }
-
     public void nextRecipeStep() {
-        incrementRecipeStep();
+        if (selectedRecipeStep.getValue() != null) {
+            setSelectedRecipeStep(selectedRecipeStep.getValue().getStepNumber() + 1);
+        }
     }
 
     public void previousRecipeStep() {
-        decrementRecipeStep();
+        if (selectedRecipeStep.getValue() != null) {
+            setSelectedRecipeStep(selectedRecipeStep.getValue().getStepNumber() - 1);
+        }
     }
 }
